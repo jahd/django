346f63c9f5feb7258f9779495376369099116dc6
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2019-12-18 17:14:07 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,48 +1,37 @@
 from django.db import models
-
-
 class Actionmenee(models.Model):
-    __source__ = "HATVP/Database/10_actions_menees.csv"
-    
-    action_menee = models.TextField()
-    action_representation_interet_id = models.IntegerField(primary_key = True, verbose_name="action_representation_interet_id")
-    action_menee_autre = models.TextField()
+    action = models.CharField(null = True,max_length = 144, verbose_name = 'action')
+    action_id = models.IntegerField(primary_key = True , default = 0, verbose_name = 'action_id')
+    action_autre = models.CharField(null = True, max_length = 144, verbose_name = 'action_autre')
 
 class Ministeres(models.Model):
-    __source__ = "HATVP/Database/13_ministeres_aai_api.csv"
-    
-    action_representation_interet_id = models.ForeignKey(Actionmenee, verbose_name="action_representation_interet_id",on_delete=models.CASCADE)
-    responsable_public = models.CharField(max_length = 150, verbose_name="responsable_public")
-    departement_ministeriel = models.CharField(max_length = 150, verbose_name="departement_ministeriel")
+    action_id = models.ForeignKey(Actionmenee,on_delete=models.CASCADE, default = 0, verbose_name = 'action_id')
+    responsable_public = models.CharField(null = True, max_length = 150, verbose_name = 'responsable_public')
+    departement_ministeriel = models.CharField(max_length = 150, verbose_name = 'departement_ministriel')
 
-class Benificiares(models.Model):
-    __source__="HATVP/Database/11_beneficiaires.csv"
-    
-    beneficiaire_action_menee = models.CharField(max_length = 155, verbose_name="beneficiaire_action_menee")
-    action_representation_interet_id = models.ForeignKey(Actionmenee, verbose_name="action_representation_interet_id",on_delete=models.CASCADE)
-    action_menee_en_propre = models.BooleanField(verbose_name="action_menee_en_propre")
-    
+class Benificiares(models.Model):   
+    beneficiaire = models.CharField(max_length = 155, null = True, verbose_name = 'beneficiaire')
+    action_id = models.ForeignKey(Actionmenee,on_delete=models.CASCADE, default = 0, verbose_name = 'action_id')
+    en_propre = models.BooleanField(null = True, blank = True, verbose_name = 'en_propre')
+
 class GeneralInformation(models.Model):
-    __source__ = "hatvp/Database/1_informations_generales.csv"
-
-    id = models.IntegerField(primary_key=True, verbose_name="representants_id")
-    address = models.CharField(max_length=256, verbose_name="adresse", blank=True)
-    zipcode = models.CharField(max_length=256, verbose_name="code_postal", blank=True)
-    last_activity = models.DateTimeField(verbose_name="derniere_publication_activite", blank=True, null=True)
-    first_activity = models.DateTimeField(verbose_name="date_premiere_publication", blank=True, null=True)
-    organizations = models.BooleanField(verbose_name="declaration_organisation_appartenance", default=False)
-    third_parties = models.BooleanField(verbose_name="declaration_tiers", default=False)
-    name = models.CharField(max_length=256, verbose_name="denomination")
-    nif = models.CharField(max_length=256, unique=True, verbose_name="identifiant_national")
-    activities = models.BooleanField(verbose_name="activites_publiees", default=False)
-    facebook = models.URLField(verbose_name="page_facebook", blank=True)
-    linkedin = models.URLField(verbose_name="page_linkedin", blank=True)
-    twitter = models.URLField(verbose_name="page_twitter", blank=True)
-    website = models.URLField(verbose_name="site_web", blank=True)
-    hatvp_name = models.CharField(max_length=256, verbose_name="nom_usage_HATVP")
-    country = models.CharField(max_length=32, verbose_name="pays")
-    acronym = models.CharField(max_length=32, verbose_name="sigle_HATVP")
-    nif_type = models.CharField(max_length=8, verbose_name="type_identifiant_national")
-    town = models.CharField(max_length=64, verbose_name="ville")
-    category = models.CharField(max_length=64, verbose_name="label_categorie_organisation")
-    
+    id = models.IntegerField(primary_key=True)
+    address = models.CharField(max_length=256, blank=True)
+    zipcode = models.CharField(max_length=256, blank=True)
+    last_activity = models.DateTimeField(blank=True, null=True)
+    first_activity = models.DateTimeField(blank=True, null=True)
+    organizations = models.BooleanField(default=False)
+    third_parties = models.BooleanField(default=False)
+    name = models.CharField(max_length=256)
+    nif = models.CharField(max_length=256, unique=True)
+    activities = models.BooleanField(default=False)
+    facebook = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    website = models.URLField(blank=True)
+    hatvp_name = models.CharField(max_length=256)
+    country = models.CharField(max_length=32)
+    acronym = models.CharField(max_length=32)
+    nif_type = models.CharField(max_length=8)
+    town = models.CharField(max_length=64)
+    category = models.CharField(max_length=64)
