715d402f5782331669bd43422b462c437cfae391
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2019-12-18 17:19:43 +0100

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Actionmenee, Ministeres, Benificiares
+
+# Register your models here.
+admin.site.register(Actionmenee)
+admin.site.register(Ministeres)
+admin.site.register(Benificiares)
