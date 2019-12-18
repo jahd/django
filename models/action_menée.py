983a74e35df12c5d3ef1f6be1995d7ec7ae5cb2f
jahd <45184003+jahd@users.noreply.github.com>
2019-12-18 17:16:57 +0100

    Delete action_menée.py

--- models/action_menée.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Actionmenee(models.Model):
-    __source__ = "HATVP/Database/10_actions_menees.csv"
-    
-    action_menee = models.TextField()
-    action_representation_interet_id = models.IntegerField(primary_key = True, verbose_name="action_representation_interet_id")
-    action_menee_autre = models.TextField()
