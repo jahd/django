e03f4eac26ae502c79e3a1405cc9f0927cfad344
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2020-01-29 15:26:57 +0100

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import HomeForm
-from .methods import methods
+from .models import Actionmenee, Ministeres, Benificiares
 
 def home(request):
     return render(request,'analytics/home.html')
@@ -13,7 +13,21 @@ def post(request):
     form = HomeForm(request.post)
     if form.is_valid():
         text = form.clean_data['recuperer']
-        id =  methods.searchdata(text)
-        args = {'obtained':methods.getdata(id)}
+        id =  searchdata(text)
+        args = {'obtained':getdata(id)}
     return render(request,'analytics/home.html',args)
+
+def searchdata (name=''):
+    obj = Benificiares.objects.filter(beneficiaire = name).first()
+    id = obj.action_id
+    return id
     
+def getdata (id):
+    obj1 = Actionmenee.objects.filter(action_id = id)
+    str1 = obj1.action
+    str2 = obj1.action_autre
+    obj2 = Ministeres.objects.filter(action_id = id)
+    str3 = obj2.responsable_public
+    str4 = obj2.departement_ministeriel
+    return str1 + str2 + str3 + str4
+   
