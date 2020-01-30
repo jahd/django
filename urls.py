77260e3d6faee09d41a2f1c608c121202d10404b
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2020-01-30 21:50:46 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+import analytics.views
+
+app_name = 'analytics'
 
 urlpatterns = [
-            path('', views.home, name = 'analytics-home'),
+            path('', analytics.views.index, name='index'),
+            path('analytics/<int:id>/', analytics.views.detail, name='detail'),
+            path('analytics/search/', analytics.views.search, name='search'),
         ]
+
 
